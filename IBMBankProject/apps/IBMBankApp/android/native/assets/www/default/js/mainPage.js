04b7685a38d9b5081ef412723850d83eddeb266b
demo <demo@192.168.194.2>
2015-10-03 18:42:55 -0400

    added android environment

---
+++ IBMBankProject/apps/IBMBankApp/android/native/assets/www/default/js/mainPage.js
@@ -0,0 +1,24 @@
+
+/* JavaScript content from js/mainPage.js in folder common */
+
+$('#mainPage').on( 'pagecreate',function(event, ui){
+	var dt = new Date();
+	dt = dt.getTime() - 36*60*60*1000;
+	dt = new Date(dt);	
+	$("#timestamp").html("Last login: " + dt.toDateString() + " " + dt.toLocaleTimeString());	
+	setTimeout(function(){
+		$('#fund_porgress').animate({width:'70%'});			
+		},500 );	
+});
+
+$("#mainPageBackBtn").on("click", function(e){
+	console.log("mainPageBackBtn - click");
+	$.mobile.changePage("#loginPage", {transition: "slide"});
+});
+
+$("#myAccountsBtn").on("click", function(e){
+	console.log("myAccountsBtn - click");
+	$.mobile.changePage("#accountPage", {transition: "slide"});
+});
+
+
---
+++ IBMBankProject/apps/IBMBankApp/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"date":1443912069055,"machine":"192.168.194.2","checksum":1215638257};
+/* Date: Sat Oct 03 18:41:09 EDT 2015 */
