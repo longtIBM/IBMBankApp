d06f9e3a9cd59988dae9b452c1f0636fd18b11aa
demo <demo@192.168.194.2>
2015-10-03 16:43:47 -0400

    generated android project

---
+++ IBMBankProject/apps/IBMBankApp/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"date":1443904350204,"machine":"192.168.194.2","checksum":1215638257};
+/* Date: Sat Oct 03 16:32:30 EDT 2015 */
---
+++ IBMBankProject/apps/IBMBankApp/android/native/gen/com/IBMBankApp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.IBMBankApp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
