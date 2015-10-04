98c35a11f75000dcb85d94ad2e7c97cb7a6f9504
demo <demo@192.168.194.2>
2015-10-03 20:12:30 -0400

    changed Username to username on login page

--- IBMBankProject/apps/IBMBankApp/android/native/assets/www/default/worklight/checksum.js
+++ IBMBankProject/apps/IBMBankApp/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1443912276133,"machine":"192.168.194.2","checksum":1215638257};
-/* Date: Sat Oct 03 18:44:36 EDT 2015 */
+var WL_CHECKSUM = {"date":1443915817106,"machine":"192.168.194.2","checksum":1631325860};
+/* Date: Sat Oct 03 19:43:37 EDT 2015 */
