374b08bdd4cc7127b7436ef70480cf8ddafd5c36
demo <demo@192.168.194.2>
2015-10-05 19:22:20 -0400

    rebuilt all environments

--- IBMBankProject/apps/IBMBankApp/android/native/assets/www/default/worklight/checksum.js
+++ IBMBankProject/apps/IBMBankApp/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1444085013271,"machine":"192.168.194.2","checksum":1631325860};
-/* Date: Mon Oct 05 18:43:33 EDT 2015 */
+var WL_CHECKSUM = {"date":1444087209621,"machine":"192.168.194.2","checksum":1215638257};
+/* Date: Mon Oct 05 19:20:09 EDT 2015 */
