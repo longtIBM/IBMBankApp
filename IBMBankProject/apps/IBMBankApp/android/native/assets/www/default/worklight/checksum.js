1d300c69928e27e00e46c9b45b989a26d1eee632
demo <demo@192.168.194.2>
2015-10-07 12:10:25 -0400

    updated adapter logic

--- IBMBankProject/apps/IBMBankApp/android/native/assets/www/default/worklight/checksum.js
+++ IBMBankProject/apps/IBMBankApp/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1444228235439,"machine":"192.168.194.2","checksum":4031918512};
-/* Date: Wed Oct 07 10:30:35 EDT 2015 */
+var WL_CHECKSUM = {"date":1444234143165,"machine":"192.168.194.2","checksum":1631325860};
+/* Date: Wed Oct 07 12:09:03 EDT 2015 */
